103da7761a08caf1e70a3c61435de3a8a5e09f3b
jinwonbaee <qowlsdnjs3@naver.com>
2024-03-18 11:19:55 +0900

    message

---
+++ Project3/Project3/4.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main (void){
+	int T;
+	int test_case;
+	
+	freopen("input.txt", "r", stdin);
+	setbuf(stdout, NULL);
+	scanf(" % d", &T);
+
+	for (test_case = 1;test_case <= T;++test_case) {
+		int num[10];
+		for (int i = 0; i < 10;i++) {
+			scanf("%d", &num[i]);
+		}
+		for (int i = 0; i < 10;i++) {
+			printf("%d", num[i]);
+		}
+	}
+	return 0;
+}
