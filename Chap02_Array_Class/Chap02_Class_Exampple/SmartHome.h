2a5b4b55fce92011a7880812f87038350ff2487d
jinwonbaee <qowlsdnjs3@naver.com>
2024-04-15 12:04:09 +0900

    m

---
+++ Chap02_Array_Class/Chap02_Class_Exampple/SmartHome.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <iostream> // input output 해주는 파일
+#include <string>  // 문자열
+using namespace std; //std:: 안써도 됨,std 라이브러리 namespace 설정
+
+// SmartHome
+class SmartHome {
+private:
+    // 클래스 변수들 ( member variable,attrubute,field)
+    string owner;
+    int temperature;
+    int humidity;
+    bool security;// 참 거짓
+public:
+    // 클래스 함수들 (class method,function)
+    // 생성자
+    SmartHome(string owner, int temperature, int humidity, bool security) {
+        this->owner = owner;
+        this->temperature = temperature;
+        this->humidity = humidity;
+        this->security = security;
+    }
+    // parameter, argument
+    // SmartHome(string o, int t, int h, bool sec)
+    //   : temperature(t), humidity(h), security(sec) {
+    // strcpy(owner, o);
+    //}
+    // 소멸자 ~SmartHome () {}
+    void setTemperature(int temperature) {
+        this->temperature = temperature;
+    }
+    void setHumidity(int humidity) {
+        this->humidity = humidity;
+    }
+    void setSecurity(bool security) {
+        this->security = security;
+    }
+    string getOwner() {
+        return this->owner;
+    }
+    int getTemperature() {
+        return this->temperature;
+    }
+    int getHumidity() {
+        return this->humidity;
+    }
+    bool getSecurity() {
+        return this->security;
+    }
+    void printStatus() {
+        cout << ": " << this->owner << endl;
+        cout << "온도: " << this->temperature << "도" << endl;
+        cout << "습도: " << this->humidity << "%" << endl;
+        if (this->security) {
+            cout << " security on" << endl;
+        }
+        else {
+            cout << "security off" << endl;
+        }
+    }
+};
+
--- Chap02_Array_Class/Chap02_Class_Exampple/main.cpp
+++ Chap02_Array_Class/Chap02_Class_Exampple/main.cpp
@@ -2,6 +2,8 @@
 //#include "SportsCar.h"
 #include <cstdio>
 #include "Rectangle.h"
+#include "SmartHome.h"
+#include "Music.h"
 int main() {
 	Rectangle r(10, 20);
 	double perimeter = r.getPerimeter();
@@ -20,4 +22,43 @@ yourCar.whereAmI();
 //SportsCar 클래스 사용
 SportsCar scar;
 scar.speedUp();*/
+
+// smartHome
+/*
+SmartHome myHome("배진원", 23, 40, true);
+myHome.printStatus()*/
+
+//Music Streaming Service 
+MusicStreamingService myService("Spotify");
+myService.addMusic("VIBE", "Taeyang", "Album", 2023);
+myService.addMusic("Ditto", "NewJeans", "Album", 2023);
+myService.addMusic("Attention", "NewJeans", "Album", 2023);
+
+string music_title;
+cout << "Enter the Music Title : ";
+cin >> music_title;
+
+Music* result = myService.searchByTitle(music_title);
+if (result != NULL) {
+	cout << "found: " << result->getTitle() << " by " << result->getArtist() << endl;
+
+}
+else {
+	cout << "Not found" << endl;
+}
+
+string artist_name;
+cout << "Enter the Artist Name : ";
+cin >> artist_name;
+
+vector<Music*> artistResult = myService.searchByArtist(artist_name);
+if (artistResult.size() > 0) {
+	cout << "Found " << artistResult.size() << " songs by " << artist_name << " : " << endl;
+	for (int i = 0; i < artistResult.size(); i++) {
+		cout << artistResult[i]->getTitle() << endl;
+	}
+}
+else {
+	cout << "not found" << endl;
+}
 
