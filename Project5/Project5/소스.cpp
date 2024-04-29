6e9fe897995758965d9685208c654998e0af6ace
jinwonbaee <qowlsdnjs3@naver.com>
2024-04-29 12:50:35 +0900

    m

--- Project5/Project5/소스.cpp
+++ Project5/Project5/소스.cpp
@@ -1,18 +1,11 @@
 #include <stdio.h>
-#define Max(a,b) ((a>b)?a:b)
-#define Max_DEGREE 50
 
-typedef struct {
-	int degree;
-	float coef[MAX_DEGREE];
-} polynomial;
 
-int main(void) {
-	polynomial A = { 3, {4,3,5,0} };
-	polynomial B = { 4, {3,1,0,2,1 } };
-	polynomial C;
 
-	C = addPoly(A, B);
-	printf("\n A(x) ="); printfPoly(A)
-	printf("\n (x) ="); printfPoly(A)
-	printf("\n A(x) ="); printfPoly(A)
+struct employee Lee, Hong, Kim[3];
+struct employee* sp;
+
+int main() {
+	printf("%c", *sp);
+}
+
